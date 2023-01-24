11b448e80652eadcc508813f6b860915afebbc40
roidsaja <roid0520@gmail.com>
2023-01-24 16:41:18 +0700

    Feat: Added confetti fx

---
+++ src/utils/confetti.ts
@@ -0,0 +1,27 @@
+import confetti from 'canvas-confetti';
+
+const defaults = {
+  spread: 360,
+  ticks: 50,
+  gravity: 0,
+  decay: 0.94,
+  startVelocity: 30,
+  shapes: ['star'],
+  colors: ['FFE400', 'FFBD00', 'E89400', 'FFCA6C', 'FDFFB8'],
+};
+
+export const shoot = () => {
+  confetti({
+    ...defaults,
+    particleCount: 40,
+    scalar: 1.2,
+    shapes: ['star'],
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 10,
+    scalar: 0.75,
+    shapes: ['circle'],
+  });
+};
