5751950388555c4b1cfa760a78b18405fedec3a2
roidsaja <roid0520@gmail.com>
2023-01-24 13:21:14 +0700

    Feat: fetch pokeapi

---
+++ src/api/fetchPokemon.ts
@@ -0,0 +1,20 @@
+import { Pokemon } from '../interfaces/Pokemon';
+
+export const fetchPokemon = async (pokemon: string) => {
+  const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+  let response;
+  let data: Pokemon | null;
+  let error;
+
+  try {
+    response = await fetch(URL);
+    data = await response.json();
+    error = false;
+  } catch {
+    data = null;
+    error = true;
+  }
+
+  return { response, data, error };
+};
